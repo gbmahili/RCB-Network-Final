8077500321e9804d0868f3e44463054ac08f7d44
gbmahili <georgesmahili@gmail.com>
2018-04-07 22:53:08 -0400

    Added new files and functionality to make the file upload

--- client/src/utils/routes.js
+++ client/src/utils/routes.js
@@ -1,11 +1,6 @@
 import axios from 'axios';
 
-const user = {
-  firstName: 'veena',
-  lastName: 'Uppal'
-};
 export default {
-  
     // this /test route is created.  this test method is called in the test.js
     test: function(){
       // this is being sent to be called when the client side server runs it shall display "hello i mv veeena"
