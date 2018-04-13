8ba3c98510e4e5310c83c606424fd2db2d768683
gbmahili <georgesmahili@gmail.com>
2018-04-12 23:39:27 -0400

    Added a component to provide the status on login and sign up

--- client/src/Components/AboutUser.js
+++ client/src/Components/AboutUser.js
@@ -7,11 +7,13 @@ class UserAbout extends React.Component {
                 <div style={{ minHeight: 400 }} className="card blue-grey lighten-1">
                     <div className="card-content white-text">
                         <span className="card-title">{this.props.firstName}</span>
-                        <p>I am a very simple card. I am good at containing small bits of information.
-                            I am convenient because I require little markup to use effectively.</p>
+                        <p>
+                            COMING SOON! <br />
+                            You can add your profession and resume below.
+                        </p>
                     </div>
                     <div className="card-action">
-                        <a href="#!">This is a link</a>
+                        <a href="#!">More</a>
                         <a href="#!">This is a link</a>
                     </div>
                 </div>
