b7ab6e24c205ffe766f55acdf3b66f657032b1f8
veena uppalapati <Veena@veenas-MBP.fios-router.home>
2018-04-22 19:41:00 -0400

    Welcome page is  media querified 1

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -4,7 +4,7 @@ class Header extends React.Component {
     render() {
         return (
             <div>
-                <h1>Welcome to RCB Network</h1>
+                <h1>Welcome to RCB Networkk</h1>
                 <h2>Where Cool Developers meet</h2>
             </div>
         );
--- client/src/Components/Home/NavBar.js
+++ client/src/Components/Home/NavBar.js
@@ -2,17 +2,20 @@ import React from 'react';
 class NavBar extends React.Component {
     render() {
         return (
-            <div className="navbar-fixed teal lighten-2">
-                <nav className="teal lighten-2" style={{ paddingLeft: 30 }}>
-                    <div className="nav-wrapper">
-                        <a href="/" className="brand-logo">RCB Network 2018</a>
-                        <ul className="right hide-on-med-and-down">
-                            <li><a href={this.props.loginOrLogoutLink}>{this.props.loginOrLogout}</a></li>
-                            <li><button className="btn btn-flat white-text" onClick={this.props.logoutFunction}><a href={this.props.signUpOrEditProfileLink}>{this.props.signUpOrName}</a></button></li>
-                        </ul>
-                    </div>
-                </nav>
-            </div> 
+            
+            <div>
+                <div className="navbar-fixed teal lighten-2">
+                    <nav className="teal lighten-2" style={{ paddingLeft: 30 }}>
+                        <div className="nav-wrapper">
+                            <a href="/" className="brand-logo">RCB Network 2018</a>
+                            <ul className="right hide-on-med-and-down">
+                                <li><a href={this.props.loginOrLogoutLink}>{this.props.loginOrLogout}</a></li>
+                                <li><button className="btn btn-flat white-text" onClick={this.props.logoutFunction}><a href={this.props.signUpOrEditProfileLink}>{this.props.signUpOrName}</a></button></li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>      
+            </div>
         );
     }
 }
