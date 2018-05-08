84b1d8394d98648e1aa785d3b4d4f3746ae0f405
gbmahili <georgesmahili@gmail.com>
2018-05-07 22:18:17 -0400

    Added sidenav to the website

--- client/src/Components/Home/NavBar.js
+++ client/src/Components/Home/NavBar.js
@@ -2,19 +2,23 @@ import React from 'react';
 class NavBar extends React.Component {
     render() {
         return (
-            
             <div>
                 <div className="navbar-fixed teal lighten-2">
                     <nav className="teal lighten-2" style={{ paddingLeft: 30 }}>
                         <div className="nav-wrapper">
                             <a href="/" className="brand-logo">RCB Network 2018</a>
+                            <a href="#!" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
                             <ul className="right hide-on-med-and-down">
                                 <li><a href={this.props.loginOrLogoutLink}>{this.props.loginOrLogout}</a></li>
                                 <li><button className="btn btn-flat white-text" onClick={this.props.logoutFunction}><a href={this.props.signUpOrEditProfileLink}>{this.props.signUpOrName}</a></button></li>
                             </ul>
                         </div>
                     </nav>
-                </div>      
+                </div>
+                <ul className="sidenav" id="mobile-demo">
+                    <li><a href={this.props.loginOrLogoutLink}>{this.props.loginOrLogout}</a></li>
+                    <li><button className="btn btn-flat white-text" onClick={this.props.logoutFunction}><a href={this.props.signUpOrEditProfileLink}>{this.props.signUpOrName}</a></button></li>
+                </ul>
             </div>
         );
     }
