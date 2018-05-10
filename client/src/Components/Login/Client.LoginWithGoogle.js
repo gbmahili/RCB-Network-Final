c290d3b8f1bf7d45e3ce80679c3ffb2be5c08dfe
veena uppalapati <Veena@veenas-MacBook-Pro.local>
2018-05-10 15:05:31 -0400

    modified the signup button to 100% in smaller devices;  modified the login instructions container so it doesnot occupy too much space

--- client/src/Components/Login/Client.LoginWithGoogle.js
+++ client/src/Components/Login/Client.LoginWithGoogle.js
@@ -3,7 +3,7 @@ class LoginWithGoogle extends React.Component {
     render() {        
         return (
             // The logic for this button is located in './../../../public/assets/js/App-Scripts'
-            <div className="g-signin2" data-onsuccess="onSignIn"></div>
+            <div id="googleBtn" className="g-signin2" data-onsuccess="onSignIn"></div>
         );
     }
 }
