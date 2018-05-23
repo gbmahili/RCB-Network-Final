7a0745705dce69586a6242d9aa88a7d60707c0ae
gbmahili <georgesmahili@gmail.com>
2018-05-23 19:17:21 -0400

    Fixed bug that prevented certain resume file types not to download

--- client/src/Components/Portfolio/Resumes.js
+++ client/src/Components/Portfolio/Resumes.js
@@ -5,9 +5,8 @@ class Resumes extends React.Component {
             // <a href={this.props.resumeLink} className="collection-item">{this.props.professionName}</a> 
             <div className="row card">
                 <div 
-                    className="col s12 m12 center btn-flat downloadBtn" 
-                    href={this.props.resumeLink}>
-                    {this.props.professionName}
+                    className="col s12 m12 center btn-flat downloadBtn">
+                    <a href={this.props.resumeLink} target="_blank">{this.props.professionName}</a>
                 </div>
                 <div className="col s12 m12 center btn-flat deleteBtn" 
                     userid={this.props.userid} 
